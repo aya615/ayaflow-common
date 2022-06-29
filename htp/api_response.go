7b5234b2030225f444f0f9fa14635f5ac2ff28df
aya1815 <aya1815@sina.com>
2022-06-29 15:25:08 +0800

    add

--- htp/api_response.go
+++ htp/api_response.go
@@ -3,6 +3,7 @@ package htp
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"strconv"
 )
@@ -28,4 +29,38 @@ func ApiResponse(w http.ResponseWriter, statusCode int, statusTxt string, data i
 	w.Header().Set("Access-Control-Allow-Methods", "*")
 	w.WriteHeader(statusCode)
 	w.Write(response)
+}
+
+func Api_ResponseErr(statusTxt interface{}) string {
+	response, err := json.Marshal(struct {
+		StatusCode int         `json:"status_code"`
+		StatusTxt  string      `json:"status_txt"`
+		Data       interface{} `json:"data"`
+	}{
+		700,
+		fmt.Sprint(statusTxt),
+		"[]",
+	})
+	if err != nil {
+		log.Println(err)
+		return fmt.Sprintf("{\"status_code\":\"%v\",\"status_txt\":\"%v\",\"data\":\"[]\"", 700, err)
+	}
+	return string(response)
+}
+
+func Api_Response(statusCode int, statusTxt string, data interface{}) string {
+	m := new(struct {
+		Status_Code int         `json:"status_code"`
+		Status_Txt  string      `json:"status_txt"`
+		Data        interface{} `json:"data"`
+	})
+	m.Data = data
+	m.Status_Code = statusCode
+	m.Status_Txt = statusTxt
+	jsonstr, err := json.Marshal(m)
+	if err != nil {
+		log.Println(err)
+		return fmt.Sprintf("{\"status_code\":\"%v\",\"status_txt\":\"%v\",\"data\":\"[]\"", 700, err)
+	}
+	return string(jsonstr)
 }
